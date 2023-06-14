a4e29143c5ecc7fc8c1af8cacfefee381d99039b
Artsiom Martynau <xzfantom@gmail.com>
2023-06-14 08:31:33 +0200

    feat: finish cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+// implement function that parses environment variables with prefix RSS_ and
+// prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+
+const prefix = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env);
+  const filteredKeys = keys.filter((key) => key.startsWith(prefix));
+
+  filteredKeys.forEach((key) => {
+    console.log(`${key}=${process.env[key]}`);
+  });
 };
 
 parseEnv();
