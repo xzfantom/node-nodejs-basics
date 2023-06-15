e8b5920327befef22044623fc0b08aa6fe6c7ba7
Artsiom Martynau <xzfantom@gmail.com>
2023-06-15 09:10:44 +0200

    feat: finish task on streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+// implement function that reads file fileToRead.txt content using Readable
+// Stream and prints it's content into process.stdout
+
+
+import { stdout } from 'process';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToRead = join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+    const readable = createReadStream(fileToRead);
+
+    readable.on('data', (chunk) => {
+        stdout.write(chunk);
+    });
+
+    readable.on('error', (err) => {
+        throw err;
+    });
 };
 
 await read();
