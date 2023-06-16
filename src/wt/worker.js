c25445765c062d28e985adb76402f1909a0f45f3
Artsiom Martynau <xzfantom@gmail.com>
2023-06-16 08:55:55 +0200

    feat: finish task on workers

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,14 @@
+//  extend given function to work with data received from main thread and
+// implement function which sends result of the computation to the main thread
+
+import { workerData, parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (workerData === 12) throw new Error('Error in worker');
+    parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
